2440d02e76383f737001cd6d5f502d4a759f665b
Brian Mo <brimo2912@gmail.com>
2018-11-24 16:57:23 -0500

    add: editorconfig and source code
    Added editorconfig formatting. Worked more on ftool, need to add testing
    and windows support. Edited requirements.txt.

--- musings/util/ftool.py
+++ musings/util/ftool.py
@@ -9,33 +9,40 @@ rm
 mkdir
 
 This module contains several importable functions to deal with files.
-We're using typing (Python >= 3.5). 
+We're using typing (Python >= 3.5).
 
-TODO: Change all print statements to log statements.
+TODO: !Change all print statements to log statements.
+      !Add windows support.
 """
 import os
 from typing import List
 from . import strtool
 
+# Module-private variable, `_sysinfo`, contains os-specific information
+# that may be useful to ftool functions.
+_sysinfo = {
+    "name": os.name
+}
 
-def _resolve_dirpath(dirpath: str, add_to_path: str = "") -> str:
+
+def resolve_dirpath(dirpath: str, add_to_path: str = "") -> str:
     """
-    Clean up dirpaths (relative and absolute).
+    Ensure a directory path is valid and absolute. If a dirpath is already
+    a valid absolute path, simply return the dirpath. The optional
+    `add_to_path` value is automatically appended to the resolved
+    absolute dirpath. If no value is specified for this optional arg,
+    add_to_path defaults to an empty string.
 
     Args:
         dirpath (str): The path.
-        add_to_path (str): A value to add to the existing path
-                           (default: str)
+        add_to_path (str): A value to add to the existing path (default: str)
     Returns:
         str: Resolved path in the form of a string.
 
     Raises:
-        ValueError: If an empty string is supplied, a ValueError is
-                    raised. If the path is invalid (path is a file) or
-                    path is not valid, this exception will also be
-                    raised.
+        ValueError: If an empty string is supplied for dirpath.
     """
-    dirpath_is_valid = strtool.expect_nonempty_str(dirpath)
+    dirpath_is_valid: bool = strtool.expect_nonempty_str(dirpath)
 
     if not dirpath_is_valid:
         raise ValueError("Argument supplied was in an invalid format.")
@@ -48,19 +55,25 @@ def _resolve_dirpath(dirpath: str, add_to_path: str = "") -> str:
     if os.path.isdir(dirpath):
         return os.path.join(dirpath, add_to_path)
 
+    # If it's not a valid directory, we should check if its a file.
+    # This allows us to give a more specific exception message.
     elif os.path.isfile(dirpath):
         raise ValueError("Dirpath was a file, not a directory")
 
+    # If it's not a directory or a file, the path was invalid.
     raise ValueError("Dirpath was not valid.")
 
 
-def _resolve_dirpaths(dirpaths: List[str], add_to_path: str = "") -> None:
+def resolve_dirpaths(dirpaths: List[str], add_to_path: str = "") -> None:
     """
-    Same functionality as _resolve_dirpaths but accepts a list of
-    paths and attempts to resolve them independently of each other.
-    However, if an additional argument, add_to_path, is supplied,
-    then we attempt to add the `add_to_path` value to each existing 
-    dirpath.
+    Ensure directory paths are valid and absolute. If a dirpath is already
+    a valid absolute path, simply return the dirpath. The optional
+    `add_to_path` value is automatically appended to the resolved
+    absolute dirpath. If no value is specified for this optional arg,
+    add_to_path defaults to an empty string.
+
+    Essentially the same functionality as resolve_dirpath but accepts
+    a list of paths and attempts to resolve them independently of each other.
 
     Args:
         dirpaths (List[str]): A list of dirpaths to resolve.
@@ -72,23 +85,25 @@ def _resolve_dirpaths(dirpaths: List[str], add_to_path: str = "") -> None:
         generate the resolved list.
 
     Raises:
-        ValueError -- Raised if the supplied argument(s) are invalid.
+        ValueError -- Raised if the supplied dirpath(s) are invalid.
     """
-    dirpaths_is_valid = strtool.expect_nonempty_str(dirpaths)
-    atp = True if add_to_path is not None else False
+    dirpaths_is_valid: str = strtool.expect_nonempty_str(dirpaths)
 
     if not dirpaths_is_valid:
         raise ValueError("Supplied argument was invalid.")
-    
-    if atp:
-        atp = strtool.expect_nonempty_str(add_to_path)
-        
+
     # If the dirpath is already absolute, we don't have
     # to do anything to it.
     for i, path in enumerate(dirpaths):
-        dirpaths[i] = os.path.abspath(path)
-        if atp:
-            dirpaths[i] = os.path.join(dirpaths[i], add_to_path)
+        abs_dirpath = os.path.abspath(path)
+        dirpaths[i] = os.path.join(abs_dirpath, add_to_path)
+
+
+def resolve_fname(fname: str) -> str:
+    """
+    Ensure a file name is in a valid format.
+    """
+    pass
 
 
 def check_file_exists(fname: str, dirpath: str) -> bool:
@@ -96,8 +111,8 @@ def check_file_exists(fname: str, dirpath: str) -> bool:
     Check if a file exists in a given directory. If it doesn't and there
     are folders within the dirpath, return False. This function does not
     recursively traverse subdirectories.
-    
-    The file name must include its extension. The dirpath may be 
+
+    The file name must include its extension. The dirpath may be
     relative to the caller function's location, but it is preferable to
     use an absolute path with the dunder, '__file__'.
 
@@ -105,7 +120,7 @@ def check_file_exists(fname: str, dirpath: str) -> bool:
         fname -- The file's name.
         dirpath -- The path to a directory. The path may be relative to
                    the caller of this function.
-    
+
     Returns:
         bool: The return value. True if the file exists, False
               otherwise.
@@ -118,12 +133,12 @@ def check_file_exists(fname: str, dirpath: str) -> bool:
     if not args_valid:
         return False
 
-    resolved_dirpath: str = _resolve_dirpath(dirpath, add_to_path=fname)
+    resolved_dirpath: str = resolve_dirpath(dirpath, add_to_path=fname)
 
     # Check if the directory is an abs or rel path.
     if not os.path.isabs(dirpath):
         resolved_dirpath = os.path.abspath(dirpath)
-    
+
     abs_filepath: str = os.path.join(resolved_dirpath, fname)
 
     # Check if directory exists
@@ -131,26 +146,27 @@ def check_file_exists(fname: str, dirpath: str) -> bool:
 
     if res_dirpath_exists:
         return True
-    
+
     return False
 
-    
-def touch(fname: str, dirpath: str = None, overwrite: bool = False) -> bool:
+
+def touch(fname: str, dirpath: str = "", overwrite: bool = False) -> bool:
     """
-    Make a file in the provided `dirpath` directory. If dirpath is not
-    a directory, an error is raised. If a dirpath is not supplied, a new
-    file is created in the same directory as the caller function's
-    module location. If overwrite is set to `True`, any file matching the
-    fname in the expected dirpath will be automatically overwritten (w).
-    If it is false and the file exists, the file will simply be appended
-    to (a).
+    Make a file in the provided `dirpath` directory.
+
+    If dirpath is supplied and not a directory, an error is raised. If a
+    dirpath is not supplied, a new file is created in the same directory
+    as the caller function's module location. If overwrite is set to `True`,
+    any file matching the fname in the expected dirpath will be automatically
+    overwritten (w). If it is false and the file exists, the file will
+    simply be appended to (a).
 
     Args:
         fname (str) -- The file name.
         dirpath (str) -- The directory in which the new file should be
                          created (default: None).
         overwrite (bool) -- Overwrite if the file already exists.
-                        
+
     Raises:
         ValueError -- If `fname` is invalid, an error is raised.
     """
@@ -160,31 +176,31 @@ def touch(fname: str, dirpath: str = None, overwrite: bool = False) -> bool:
 
     if not fname_valid:
         raise ValueError("The file name supplied was invalid.")
-    
+
     # we have a relative path. raise an error.
-    # TODO: Create missing directories when a relative path is supplied
+    # TODO: ?Create missing directories when a relative path is supplied
     #       as a file name.
     if "/" in fname:
         raise ValueError("Supplied fname cannot have '/' characters.")
 
     if dirpath is not None:
         try:
-            resolved_dirpath = _resolve_dirpath(dirpath)
+            resolved_dirpath = resolve_dirpath(dirpath)
         except ValueError as ve:
             # Log value error.
-            print(ve) 
+            print(ve)
     else:
         resolved_dirpath = os.getcwd()
-    
+
     resolved_dirpath = os.path.join(resolved_dirpath, fname)
-    
+
     mode = "w" if overwrite else "a"
     with open(resolved_dirpath, mode):
         os.utime(resolved_dirpath, None)
-    
+
     return True
 
-    
+
 def rm(fname: str, dirpath: str = None) -> bool:
     pass
 
